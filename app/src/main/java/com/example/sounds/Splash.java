e760d74ced3324860ede71dbdd9c88cc36056bf8
LuisMonedero <luis.monedero8@gmail.com>
2020-11-04 14:42:19 +0100

    Added Splash

---
+++ app/src/main/java/com/example/sounds/Splash.java
@@ -0,0 +1,23 @@
+package com.example.sounds;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_activity);
+
+        new Handler().postDelayed(new Runnable() {
+            public void run() {
+                Intent intent = new Intent(Splash.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            };
+        }, 5000);
+    }
+}
